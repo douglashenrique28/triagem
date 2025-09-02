cadffdfee4998119f9964314478fb29052d9ba26
douglashenrique28 <ribeiro_henrique.douglas@hotmail.com>
2025-09-02 12:35:23 -0300

    Add files via upload

---
+++ triagem-main/triagem/cadastro.php
@@ -0,0 +1,62 @@
+<?php
+
+include "conexao.php";
+$nome = $_POST["nome"];
+$idade = $_POST ["idade"];
+$sexo = $_POST ["sexo"];
+$data_de_nascimento = $_POST ["data_de_nascimento"];
+$cpf = $_POST ["cpf"];
+$telefone = $_POST ["telefone"];
+$endereco = $_POST ["endereco"];
+
+$sintomas = $_POST ["sintomas"];
+$inicio_dos_sintomas = $_POST ["inicio_dos_sintomas"];
+$doencas_pre_existentes = $_POST ["doencas_pre_existentes"];
+$uso_de_medicamentos = $_POST ["uso_de_medicamentos"];
+$alergias = $_POST ["alegias"];
+
+$pressao_arterial = $_POST ["pressao_arterial"];
+$temperatura = $_POST ["temperatura"];
+$frenquencia_cardiaca = $_POST ["frenquencia_cardiaca"];
+$saturacao = $_POST ["saturacao"];
+
+$observacoes = $_POST ["observacoes"];
+
+$sql = "INSERT INTO triagem(nome, idade, sexo, data_de_nascimento, cpf, telefone, endereco, sintomas, 
+inicio_dos_sintomas, doencas_pre_existentes, uso_de_medicamentos, alergias, pressao_arterial, temperatura, 
+frequencia_cardiaca, saturacao, observacoes)
+
+VALUES('$nome', '$idade', '$sexo', '$data_de_nascimento', '$cpf',
+'$telefone', '$endereco', '$sintomas', '$inicio_dos_sintomas', '$doencas_pre_existentes', '$uso_de_medicamentos',
+'$alergias', '$pressao_arterial', '$temperatura', '$frenquencia_cardiaca', '$saturacao', '$observacoe')";
+
+?>
+
+<?php
+        if($coon -> query($sql) == TRUE) {
+            echo"<h2> Paciente Cadastrado com Sucesso </h2>";
+            echo "<div class= 'paciente'> </div>";
+            echo "<b> Nome: </b> $nome <br>";
+            echo "<b> Idade: </b> $idade <br>";
+            echo "<b> Sexo: </b> $sexo <br>";
+            echo "<b> Data_de_nascimento: </b> $data_de_nascimento <br>";
+            echo "<b> Cpf: </br> $cpf <br>";
+            echo "<b> Telefone: </b> $telefone <br>";
+            echo "<b> Endereco: </b> $endereco <br>";
+            echo "<b> Sintomas: </b> $sintomas <br>";
+            echo "<b> Incio_dos_Sintomas: </b> $inicio_dos_sintomas <br>";
+            echo "<b> Sintomas_pre_existentes: </b> $sintomas_pre_existentes <br>";
+            echo "<b> Uso_de_medicamentos: </b> $uso_de_medicamentos <br>";
+            echo "<b> Alergias: </b> $alergias <br>";
+            echo "<b> Pressao_arterial: </b> $pressao_arterial <br>";
+            echo "<b> Temperatura: </b> $temperatura <br>";
+            echo "<b> Frequencia_cardiaca: </b> $frequencia_cardiaca <br>";
+            echo "<b> Saturação: </b> $saturacao <br>";
+            echo "<b> Observacões: </b> $observacoes <br>";
+
+        }else{
+            echo "<p> Erro ao cadastrar paciente" .
+            $coon -> error . "</p>";
+        }
+        $coon -> close ();
+    ?>
---
+++ triagem-main/triagem/triagem.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE triagem;
+USE triagem;
+
+CREATE TABLE pacientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (150) NOT NULL,
+    idade INT NOT NULL,
+    cpf VARCHAR (20) NOT NULL,
+    sexo VARCHAR (10) NOT NULL,
+    data_nascimento DATE, 
+    telefone VARCHAR (20) NOT NULL,
+    endereco VARCHAR (200) NOT NULL,
+    sintomas VARCHAR (30) NOT NULL,
+    inicio_dos_sintomas DATE,
+    doencas_pre_existentes VARCHAR (30) NOT NULL,
+    uso_de_medicamentos VARCHAR (20) NOT NULL,
+    alergias VARCHAR (20) NOT NULL,
+    pressao_arterial VARCHAR (10) NOT NULL,
+    temperatura VARCHAR (10) NOT NULL,
+    frequencia_cardiaca VARCHAR (10) NOT NULL,
+    saturacao VARCHAR (10) NOT NULL,
+
+    observacao TEXT NOT NULL
+
+);
